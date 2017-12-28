f6cd3d68303b652d48f75f9012988a006f777e18
Virendra Kumar Tiwari <vtiwari@ishir.com>
2017-12-28 20:32:07 +0530

    first commit

---
+++ config/constants.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'project_name':'express-mvc-generator',
+	'adminname': 'Virendra',
+	'adminmail': 'vtiwari@ishir.com',
+	'host':"http://localhost:8042",
+	'email_smtp_host':'smtp.office365.com',
+	'email_smtp_port':'587',
+	"smtp_from_eamil":"vtiwari@ishir.com",
+	"smtp_from_name":"express-mvc-generator",
+	"alert_email":"vtiwari@ishir.com",
+	"alert_email_name":"Virendra",
+};
---
+++ config/routes.js
@@ -0,0 +1,35 @@
+var home = require('../app/controllers/home');
+
+//you can include all your controllers
+
+module.exports = function (app, passport) {
+
+    app.get('/login', home.login);
+    app.get('/signup', home.signup);
+
+    app.get('/', home.loggedIn, home.home);//home
+    app.get('/home', home.loggedIn, home.home);//home
+
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/home', // redirect to the secure profile section
+        failureRedirect: '/signup', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
+    }));
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect: '/home', // redirect to the secure profile section
+        failureRedirect: '/login', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
+    }));
+    // =====================================
+    // PROFILE SECTION =====================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
+    //app.get('/profile', profile.loggedIn, profile.profile);    
+    // =====================================
+    // LOGOUT ==============================
+    // =====================================
+    
+    app.get('/logout', home.logout);    
+}
